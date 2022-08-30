2536d7bbf12656482c90e81f3cdf8be6c175c0ab
Nhat Anh <Nhat Anh@NHATANH>
2022-08-29 18:37:09 -0700

    Add project files.

---
+++ Assignment2/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            TempData["Name"] = "Nguyen Duc Hung";
+            TempData["Age"] = "27 years old";
+            TempData["Address"] = "Ha Noi";
+            return RedirectToAction("Showdata");
+            //return View();
+        }
+
+        public ActionResult ShowData()
+        {
+            //return RedirectToAction("Index");
+            return View();
+        }
+    }
+}
